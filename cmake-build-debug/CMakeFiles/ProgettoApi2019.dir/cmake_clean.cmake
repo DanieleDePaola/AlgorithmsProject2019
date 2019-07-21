8ee2abfe0ed09509355b8f7d34082fde0d54421f
DanieleDePaola <daniele.depaola@mail.polimi.it>
2019-07-21 12:53:12 +0200

    First reading, to test in posix

---
+++ cmake-build-debug/CMakeFiles/ProgettoApi2019.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgettoApi2019.dir/main.c.obj"
+  "ProgettoApi2019.pdb"
+  "ProgettoApi2019.exe"
+  "ProgettoApi2019.exe.manifest"
+  "libProgettoApi2019.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProgettoApi2019.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
