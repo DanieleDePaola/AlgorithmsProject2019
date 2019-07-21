4ee51bfd1db5f41f08f9d252214d642a4fb98037
DanieleDePaola <daniele.depaola@mail.polimi.it>
2019-07-21 23:02:07 +0200

    command reading implement, begin to work on hash table

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 #define HASHDEFAULTDIM 2048
 #define BUFFERMAXDIM 1000
-
+#define DIMENTITYHASHTABLE 1024
 
 //GLOBAL VARIABLEs AND PROTOTYPES
 void readingInput();
@@ -52,6 +52,15 @@ typedef struct {
 
 char * buffer;
 
+void addEntity(char* nameEntity);
+void deleteEntity(char* nameEntity);
+void findName(char* stringLocation, char* buffer);
+
+//HashTable structures
+
+entityBoxHashed*  entityHashTable[DIMENTITYHASHTABLE];
+
+
 
 
 
@@ -62,57 +71,96 @@ int main() {
 
 
     //Data acquisition
-//    while (fscanf(stdin, "%s %s %s",  &command, &instructionOne, &instructionTwo)==1||fscanf(stdin, "%s" "%s" "%s",  &command, &instructionOne, &instructionTwo)==2||fscanf(stdin, "%s" "%s" "%s",  &command, &instructionOne, &instructionTwo)==3){
-//        printf(command);
-//        printf(instructionOne);
-//        printf(instructionTwo);
-//        printf("\n");
-//        fflush(stdout);
-//
-//    }
 
-    char* string = malloc(BUFFERMAXDIM);
-    printf("ciao bello");
-    while(fgets(string,BUFFERMAXDIM, stdin)){
-        printf("%s", string);
+    char* buffer = malloc(BUFFERMAXDIM);
+    char* command = malloc(10);
+    char* entityName = malloc(100);
+
+    while(fgets(buffer,BUFFERMAXDIM, stdin)){
+
+        sscanf(buffer, "%s",command);
+        if(!strcmp(command, "addent")) {
+            findName(entityName, buffer);
+            addEntity(entityName);
+        }
+        else if (!strcmp(command, "delent")) {
+            deleteEntity(entityName);
+        }
+        else if (!strcmp(command, "report")) {
+            deleteEntity(entityName);
+        }
+        else if (!strcmp(command, "addrel")) {
+            deleteEntity(entityName);
+        }
+        else if (!strcmp(command, "delrel")) {
+            deleteEntity(entityName);
+        }
 
     }
     return 0;
 
 
-    //Other functions
 
-    //TODO function used to hash a string
-    //void hashValue(char* valueToInsert, hashTableEntity);
-
-    //TODO function used to delete an entity
-    void deleteEntity(entity* nameEntity);
-
-    //TODO function used to delete a relation
-    void deleteRelation(relation* nameRelation);
-
-    //TODO function used to add a relation
-    void addRelation(char * nameRelation);
-
-    //TODO  function used to add an entity
-    void addEntity(char* nameEntity);
-
-    //TODO function used to explore the leaderboard and extract the winners
-    void report();
-
-    //TODO function used to perform a rehashing
-    //void rehash(hashtableEntity);
-
-    //TODO function used to insert an element at the edge of a list
-    void listInsertHead();
-
-    //TODO function used to delete a list Head
-    void listDeleteHead();
-
-    //TODO function used to find a winner in the leaderboard fo the single relation, it is used by report()
-    //listName findWinnerRelation();
 
 
 
 
 }
+
+//Other functions
+
+//TODO functiion that takes a global variable an retaes an hash table;
+//    void hashCreate(){
+//
+//    }
+
+void findName(char* stringLocation, char* buffer){
+    int indexBuffer=0;
+    int indexMemory=0;
+    int flag=0;
+    while(buffer[indexBuffer]!='\0'){
+        if(buffer[indexBuffer]=='"'&&!flag) flag=1;
+        else if(buffer[indexBuffer]=='"'&&!flag)flag=0;
+        if(buffer[indexBuffer]!='"'&&buffer[indexBuffer]!='\n'&&buffer[indexBuffer]&&flag){
+            stringLocation[indexMemory]=buffer[indexBuffer];
+            indexMemory++;
+        }
+
+        indexBuffer++;
+    }
+    stringLocation[indexMemory]='\0';
+}
+
+//TODO function used to hash a string
+//void hashValue(char* valueToInsert, hashTableEntity);
+
+//TODO function used to delete an entity
+void deleteEntity(char* nameEntity){
+    printf("Deleting entity");
+}
+
+//TODO function used to delete a relation
+void deleteRelation(relation* nameRelation);
+
+//TODO function used to add a relation
+void addRelation(char * nameRelation);
+
+//TODO  function used to add an entity
+void addEntity(char* nameEntity){
+
+}
+
+//TODO function used to explore the leaderboard and extract the winners
+void report();
+
+//TODO function used to perform a rehashing
+//void rehash(hashtableEntity);
+
+//TODO function used to insert an element at the edge of a list
+void listInsertHead();
+
+//TODO function used to delete a list Head
+void listDeleteHead();
+
+//TODO function used to find a winner in the leaderboard fo the single relation, it is used by report()
+//listName findWinnerRelation();
